5dc6735bba35d9d661130cbcf0293a910f48b0c5
Nekkojira <nekkojira@gmail.com>
2019-03-22 18:06:53 -0700

    gitflow-feature-stash: test-setup

--- src/test/java/com/nekkojira/gradev/GradevApplicationTests.java
+++ src/test/java/com/nekkojira/gradev/GradevApplicationTests.java
@@ -1,16 +1,24 @@
 package com.nekkojira.gradev;
 
+import com.nekkojira.gradev.web.controller.GradevController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GradevApplicationTests {
 
+	@Autowired
+	private GradevController gradevController;
+
 	@Test
 	public void contextLoads() {
+		assertThat(gradevController).isNotNull();
 	}
 
 }
